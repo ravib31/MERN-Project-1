dc743d9b3e32d9563482578da043a76f621747fd
Ravi Bhaskar <ravi.bhaskar@tecvololabs.com>
2024-01-16 12:58:22 +0530

    backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middleware/error");
+app.use(express.json());
+app.use(cookieParser())
+
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+// const { error } = require("console");
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+
+// MIddleware for Error
+app.use(errorMiddleware);
+
+module.exports = app;
